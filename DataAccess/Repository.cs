72b905dcf2355455853d6a62a28dfc753e2f0dd0
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-11-04 13:15:42 +0100

    Added search function  #4

---
+++ DataAccess/Repository.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess
+{
+    public class Repository
+    {
+        NorthwindContext context = new NorthwindContext();
+
+        public List<Employees> GetAllEmployees()
+        {
+            return context.Employees.ToList();
+        }
+    }
+}
--- Desktop/ViewModels/HRViewModel.cs
+++ Desktop/ViewModels/HRViewModel.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using DataAccess;
+using Entities.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace Desktop.ViewModels
 {
     public class HRViewModel : BaseViewModel
     {
+        private Repository repository = new Repository();
+        private List<Employees> AllEmployees { get; set; }
+        public ObservableCollection<Employees> EmployeeResults { get; set; }
+
+        public HRViewModel()
+        {
+            AllEmployees = repository.GetAllEmployees();
+            EmployeeResults = new ObservableCollection<Employees>(AllEmployees);
+            Search("an");
+        }
+
+        public void Search(string searched)
+        {
+            searched = searched.ToLower();
+
+            List<Employees> results = new List<Employees>();
+            results = AllEmployees.Where((x) => {
+                string initials = x.FirstName.ToLower().Substring(0, 2) + x.LastName.ToLower().Substring(0, 2);
+                return initials.Contains(searched);
+                }).ToList();
+
+            List<Employees> results2 = AllEmployees.Where((x) => {
+                string fullName = x.FirstName.ToLower() + " " + x.LastName.ToLower();
+                string initials = x.FirstName.ToLower().Substring(0, 2) + x.LastName.ToLower().Substring(0, 2);
+                return !(initials.Contains(searched)) && fullName.Contains(searched);
+            }).ToList();
+
+            foreach(Employees e in results2)
+            {
+                results.Add(e);
+            }
+
+            EmployeeResults = new ObservableCollection<Employees>(results);
+        }
     }
 }
