fb4d8feeb39b893c8ddd40a7e46e30ad067a857e
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-11-11 13:11:30 +0100

    Sorts orders with newest requiredDate on top #21

--- Desktop/ViewModels/CustomerInformationViewModel.cs
+++ Desktop/ViewModels/CustomerInformationViewModel.cs
@@ -16,6 +16,14 @@ namespace Desktop.ViewModels
             mainViewModel = mainVM;
             Customer = customer;
             SelectedOrders = customer.Orders.ToList();
+            SelectedOrders.Sort((a, b) =>
+            {
+                if (a.RequiredDate != null && b.RequiredDate != null)
+                {
+                    return DateTime.Compare((DateTime)b.RequiredDate, (DateTime)a.RequiredDate);
+                }
+                else return 0;
+            });
         }
     }
 }
