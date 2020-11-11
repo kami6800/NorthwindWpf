491aa2d5b632661a1773d9988e131f53f0dd55d6
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-11-11 11:40:55 +0100

    Revert "Display searchable list of customers, with ability to select #19 #20"
    This reverts commit 2b6f40f71fb6a5493ab6f92e711feb6e73e3e76b.

--- Desktop/Commands/UpdateViewCommand.cs
+++ Desktop/Commands/UpdateViewCommand.cs
@@ -38,7 +38,7 @@ namespace Desktop.Commands
                     ViewModel.SelectedViewModel = new AccountingViewModel();
                     break;
                 case "Customers":
-                    ViewModel.SelectedViewModel = new CustomersViewModel(ViewModel);
+                    ViewModel.SelectedViewModel = new CustomersViewModel();
                     break;
                 case "Products":
                     ViewModel.SelectedViewModel = new ProductsViewModel();
--- Desktop/ViewModels/CustomersViewModel.cs
+++ Desktop/ViewModels/CustomersViewModel.cs
@@ -1,11 +1,9 @@
 ﻿using Desktop.Commands;
-using Desktop.Views;
 using Entities.Models;
 using Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Text;
 
 namespace Desktop.ViewModels
@@ -33,32 +31,6 @@ namespace Desktop.ViewModels
         public SelectCustomerCommand SelectCommand { get; set; }
         public SearchCommand SearchCustomersCommand { get; set; }
         public List<Customers> AllCustomers { get; set; }
-        public ObservableCollection<Customers> CustomerResults
-        {
-            get { return _customerResults; }
-            set
-            {
-                _customerResults = value;
-                RaisePropertyChanged("CustomerResults");
-            }
-        }
-        private ObservableCollection<Customers> _customerResults;
-
-        public CustomersViewModel(MainViewModel mainView)
-        {
-            SelectCommand = new SelectCustomerCommand(mainView);
-            SearchCustomersCommand = new SearchCommand(Search);
-            AllCustomers = webService.GetAllCustomers();
-            CustomerResults = new ObservableCollection<Customers>(AllCustomers);
-        }
-
-        public void Search(string searched)
-        {
-            searched = searched.ToLower();
-            List<Customers> result = AllCustomers
-                .Where(x => x.CustomerId.ToLower().Contains(searched)).ToList();
-
-            CustomerResults = new ObservableCollection<Customers>(result);
-        }
+        public ObservableCollection<Customers> CustomerResults { get; set; }
     }
 }
