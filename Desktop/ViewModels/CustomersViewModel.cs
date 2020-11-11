c5b091cd9b75bcecea3b3328283a485793eb6ddd
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-11-11 11:45:40 +0100

    TestCommit

--- Desktop/ViewModels/CustomersViewModel.cs
+++ Desktop/ViewModels/CustomersViewModel.cs
@@ -12,24 +12,7 @@ namespace Desktop.ViewModels
 {
     public class CustomersViewModel : BaseViewModel
     {
-        /*private DataWebService webService = new DataWebService();
-        //private MainViewModel MainView { get; set; }
-        public SelectEmployeeCommand UpdateView { get; set; }
-        public SearchCommand SearchEmployees { get; set; }
-        //private Repository repository = new Repository();
-        private List<Employees> AllEmployees { get; set; }
-        private ObservableCollection<Employees> _employeeResults;
-        public ObservableCollection<Employees> EmployeeResults
-        {
-            get { return _employeeResults; }
-            set
-            {
-                _employeeResults = value;
-                RaisePropertyChanged("EmployeeResults");
-            }
-        }*/
         private DataWebService webService = new DataWebService();
-        //private MainViewModel mainView;
         public SelectCustomerCommand SelectCommand { get; set; }
         public SearchCommand SearchCustomersCommand { get; set; }
         public List<Customers> AllCustomers { get; set; }
