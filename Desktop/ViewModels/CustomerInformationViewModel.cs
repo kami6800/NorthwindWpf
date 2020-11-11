3e72715b32868d1a9b01121fdab75a60d8d243cf
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-11-11 12:09:54 +0100

    Show order, required and shipped date on orders #22

--- Desktop/ViewModels/CustomerInformationViewModel.cs
+++ Desktop/ViewModels/CustomerInformationViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using Entities.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Desktop.ViewModels
 {
     public class CustomerInformationViewModel : BaseViewModel
     {
-        public CustomerInformationViewModel(Customers customer, MainViewModel mainView)
+        private MainViewModel mainViewModel;
+        public Customers Customer { get; set; }
+        public List<Orders> SelectedOrders { get; set; }
+        public CustomerInformationViewModel(Customers customer, MainViewModel mainVM)
         {
-
+            mainViewModel = mainVM;
+            Customer = customer;
+            SelectedOrders = customer.Orders.ToList();
         }
     }
 }
