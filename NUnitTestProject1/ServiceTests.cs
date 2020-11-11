049faf465d5c8dffad938048f7c397ec7ed63aa2
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-11-11 13:41:47 +0100

    Unit testing api get functions #12

--- NUnitTestProject1/ServiceTests.cs
+++ NUnitTestProject1/ServiceTests.cs
@@ -3,38 +3,33 @@ using Services;
 
 namespace NUnitTestProject1
 {
-    public class Tests
+    public class ServiceTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        //[Test]
+        //public void LanguageFilterTest()
+        //{
+        //    ValidationWebService services = new ValidationWebService();
 
-        [Test]
-        public void LanguageFilterTest()
-        {
-            ValidationWebService services = new ValidationWebService();
+        //    string text = "Fuck you";
+        //    string expected = "**** you";
 
-            string text = "Fuck you";
-            string expected = "**** you";
+        //    string result = services.ApplyLanguageFilter(text);
 
-            string result = services.ApplyLanguageFilter(text);
+        //    Assert.AreEqual(expected, result);
+        //}
 
-            Assert.AreEqual(expected, result);
-        }
+        //[Test]
+        //public void PhoneValidationTest()
+        //{
+        //    ValidationWebService services = new ValidationWebService();
+        //    string validNumber = "+4588888888";
+        //    string invalidNumber = "+45588888888";
 
-        [Test]
-        public void PhoneValidationTest()
-        {
-            ValidationWebService services = new ValidationWebService();
-            string validNumber = "+4588888888";
-            string invalidNumber = "+45588888888";
+        //    bool valid = services.ValidPhoneNumber(validNumber);
+        //    bool invalid = services.ValidPhoneNumber(invalidNumber);
 
-            bool valid = services.ValidPhoneNumber(validNumber);
-            bool invalid = services.ValidPhoneNumber(invalidNumber);
-
-            Assert.IsTrue(valid);
-            Assert.IsFalse(invalid);
-        }
+        //    Assert.IsTrue(valid);
+        //    Assert.IsFalse(invalid);
+        //}
     }
 }
