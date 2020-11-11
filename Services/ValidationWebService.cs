62deefa773f38dbacd91d6fa52b1669e556a6311
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-11-11 08:43:44 +0100

    Added phone validation method, and unit test #14 #15

--- Services/ValidationWebService.cs
+++ Services/ValidationWebService.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Models;
 using Newtonsoft.Json;
+using Services.PhoneValidation;
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -15,5 +16,14 @@ namespace Services
             PurgoMalum purgo = JsonConvert.DeserializeObject<PurgoMalum>(json);
             return purgo.Result;
         }
+
+        public bool ValidPhoneNumber(string number)
+        {
+            string url = $"http://apilayer.net/api/validate?access_key=6ea1e9c3cf840443cd49437c55fae187&number=" + number;
+            string json = GetJson(url);
+            Numverify num = JsonConvert.DeserializeObject<Numverify>(json);
+            return num.Valid;
+        }
     }
 }
+
